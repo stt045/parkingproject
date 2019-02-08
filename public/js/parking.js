772221f36640afd532c66ebd883de6449dc1320d
Steven Tran <steven@Stevens-MacBook-Pro-3.local>
2019-02-08 11:40:18 -0800

    Added jQuery and worked on json parsing

---
+++ public/js/parking.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+function initializePage() {
+	console.log("Javascript Connected!");
+}
