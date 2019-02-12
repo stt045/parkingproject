f8400347fc0fee5f70c63a6601faeaad7a122306
Steven Tran <steven@Stevens-MacBook-Pro-3.local>
2019-02-11 23:24:48 -0800

    completed user state feedback for page status

--- public/js/parking.js
+++ public/js/parking.js
@@ -3,22 +3,6 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
-
-
-	/*
-	 * Highlights footer element when you go to different screens
-	 */
-	$("table th").click(function(e) {
-		var color = $( this ).css("background-color");
-		console.log("footer clicked. Color:" + color);
-		// If current color is white, turn to something else
-		if(color == "rgba(0, 0, 0, 0)" || color == "rgb(255, 255, 255)") {
-			$(".options li").css("background", "white");
-			$(this).css("background", "yellow");
-		} else {
-			$(this).css("background", "white");
-		}
-	})
 	
 	/*
 	 * This function highlights the parking structure the user selects. This is an indication to the user
