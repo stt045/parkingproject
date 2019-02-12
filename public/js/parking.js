57acf72683851a2056c5631535e08e7599671a1f
Steven Tran <steven@Stevens-MacBook-Pro-3.local>
2019-02-11 21:49:58 -0800

    user feedback when changing screens

--- public/js/parking.js
+++ public/js/parking.js
@@ -4,6 +4,22 @@
 $(document).ready(function() {
 	initializePage();
 
+
+	/*
+	 * Highlights footer element when you go to different screens
+	 */
+	$("table th").click(function(e) {
+		var color = $( this ).css("background-color");
+		console.log("footer clicked. Color:" + color);
+		// If current color is white, turn to something else
+		if(color == "rgba(0, 0, 0, 0)" || color == "rgb(255, 255, 255)") {
+			$(".options li").css("background", "white");
+			$(this).css("background", "yellow");
+		} else {
+			$(this).css("background", "white");
+		}
+	})
+	
 	/*
 	 * This function highlights the parking structure the user selects. This is an indication to the user
 	 * to know which one he has selected. 
