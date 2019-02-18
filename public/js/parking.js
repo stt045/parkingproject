4138b345d95a329d0447d4937a942671a1b043ea
Steven Tran <steven@Stevens-MacBook-Pro-3.local>
2019-02-17 23:46:30 -0800

    added alert for parking options

--- public/js/parking.js
+++ public/js/parking.js
@@ -8,14 +8,14 @@ $(document).ready(function() {
 	 * This function highlights the parking structure the user selects. This is an indication to the user
 	 * to know which one he has selected. 
 	 */
-	$(".options li, .settings th, .permits li").click(function() {
+	$(".settings th, .permits li").click(function() {
 		console.log("listitem clicked");
 		var color = $( this ).css("background-color");
 
 		// If current color is white, turn to something else
 		if(color == "rgba(0, 0, 0, 0)" || color == "rgb(255, 255, 255)") {
-			$(".options li, .settings th, .permits li").css("background", "white");
-			$(".options li, .settings th, .permits li").css("color", "black");
+			$(".settings th, .permits li").css("background", "white");
+			$(".settings th, .permits li").css("color", "black");
 			$(this).css("background", "#4CAF50");
 			$(this).css('color', 'white');
 		} else {
@@ -23,6 +23,25 @@ $(document).ready(function() {
 			$(this).css('color', 'black');
 		}
 	})
+
+	// Specifically for parking alerts
+	$(".options li").click(function() {
+		console.log("listitem clicked");
+		var color = $( this ).css("background-color");
+
+		// If current color is white, turn to something else
+		if(color == "rgba(0, 0, 0, 0)" || color == "rgb(255, 255, 255)") {
+			$(".options li").css("background", "white");
+			$(".options li").css("color", "black");
+			$(this).css("background", "#4CAF50");
+			$(this).css('color', 'white');
+			alert("You will now be receiving notifications for " + $(this, ".inner").text());
+		} else {
+			$(this).css("background", "white");
+			$(this).css('color', 'black');
+		}
+	})
+
 
 	$(".dropdown-content a").click(function() {
 		console.log("clicked");
