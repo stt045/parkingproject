668454b1eceb545bfedec809165cf1686c6eb4dc
Steven Tran <steven@Stevens-MacBook-Pro-3.local>
2019-02-18 23:39:16 -0800

    customized alerts to display parking lot name

--- public/js/parking.js
+++ public/js/parking.js
@@ -4,6 +4,8 @@
 $(document).ready(function() {
 	initializePage();
 	
+	var currLot = "";
+
 	/*
 	 * This function highlights the parking structure the user selects. This is an indication to the user
 	 * to know which one he has selected. 
@@ -28,6 +30,7 @@ $(document).ready(function() {
 	$(".options li").click(function() {
 		console.log("listitem clicked");
 		var color = $( this ).css("background-color");
+		console.log(is(this));
 
 		// If current color is white, turn to something else
 		if(color == "rgba(0, 0, 0, 0)" || color == "rgb(255, 255, 255)") {
@@ -35,26 +38,29 @@ $(document).ready(function() {
 			$(".options li").css("color", "black");
 			$(this).css("background", "#4CAF50");
 			$(this).css('color', 'white');
-			//alert("You will now be receiving notifications for " + $(this, ".inner").text());
+
+			// Assign current lot
+			currLot = $(this).find("#name").text();
+			console.log(currLot);
+			var message = "Now receiving alerts for " + currLot;
 			
 			// Parking alerts		
 			swal({
 			  	title: "Parking Alerts",
-			  	text: "Are you sure you want Parking alerts?",
+			  	text: message,
 			  	icon: "warning",
 			  	buttons: true,
 			  	dangerMode: true,
 			})
 			.then((willDelete) => {
 			  	if (willDelete) { // Pressed OK
-			    	
+			    	// Store parking lot value
 			  	} else { // Pressed Cancel
 			    	$(this).css("background", "white");
 					$(this).css('color', 'black');
 			  	}
 			});
 			// End parking alert
-
 		} else {
 			$(this).css("background", "white");
 			$(this).css('color', 'black');
