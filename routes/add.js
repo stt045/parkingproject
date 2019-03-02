6aa46d90ee4363e1d773f451cedc0d8b2002013e
Steven Tran <steven@Stevens-MacBook-Pro-3.local>
2019-03-01 18:00:09 -0800

    google analytics in reminders

--- routes/add.js
+++ routes/add.js
@@ -1,15 +1,27 @@
 var data = require("../data.json");
 
 exports.addReminder = function(req, res) {    
-	// Your code goes here
+	data["viewAlt"] = false;
+
 	var newReminder = {
 		"reminderTime": req.query.reminder,
 		"classTime": req.query.time,
 		"day": req.query.day
 	};
 	
-	console.log(req.query.time);
 	data.reminders.push(newReminder);
-	console.log(newReminder);
 	res.render('reminders', data);
  }
+
+exports.viewAlt = function(req, res){
+	data["viewAlt"] = true;
+
+	var newReminder = {
+		"reminderTime": req.query.reminder,
+		"classTime": req.query.time,
+		"day": req.query.day
+	};
+	
+	data.reminders.push(newReminder);
+  	res.render('viewAlt', data);
+};
--- routes/reminders.js
+++ routes/reminders.js
@@ -4,9 +4,11 @@ var data = require("../data.json");
  * GET reminders page.
  */
 exports.view = function(req, res){
+	data["viewAlt"] = false;
   	res.render('reminders', data);
 };
 
 exports.viewAlt = function(req, res){
+	data["viewAlt"] = true;
   	res.render('viewAlt', data);
 };
