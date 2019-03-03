6f3cfa2117b4fb741ff7a86d2048fc2e56aa7ed7
Steven Tran <steven@Stevens-MacBook-Pro-3.local>
2019-03-03 00:41:37 -0800

    auto

--- routes/add.js
+++ routes/add.js
@@ -13,15 +13,3 @@ exports.addReminder = function(req, res) { 
 	res.render('reminders', data);
  }
 
-exports.viewAlt = function(req, res){
-	data["viewAlt"] = true;
-
-	var newReminder = {
-		"reminderTime": req.query.reminder,
-		"classTime": req.query.time,
-		"day": req.query.day
-	};
-	
-	data.reminders.push(newReminder);
-  	res.render('viewAlt', data);
-};
